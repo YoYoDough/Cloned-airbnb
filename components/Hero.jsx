f628d57c2704dc5297fd5dbde99e1cd2bea6f209
YoYoDough <yosuf818s@gmail.com>
2024-09-18 00:14:47 -0700

    Making a clone of the airbnb site.

---
+++ components/Hero.jsx
@@ -0,0 +1,12 @@
+
+const Hero = () => {
+  return (
+    <section className = "heroSection">
+        <img src = "./photoGrid.png" className = "heroImage"></img>
+        <h1 className = "heroHeader">Online Experiences</h1>
+        <p className = "heroDesc">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+    </section>
+  )
+}
+
+export default Hero
---
+++ components/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+  return (
+    <nav className = "navbar">
+        <img src = "./airbnbLogo.png" alt = "airbnbLogo" className = "logo"></img>
+    </nav>
+  )
+}
+
+export default Navbar
